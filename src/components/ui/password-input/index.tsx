495c052d5221918c96f9aba9c6394eb077c4bf65
claudiobzrr <claudio.bezerra1998@gmail.com>
2025-11-24 14:29:10 -0300

    feat: update PasswordInput component styles and add global dark background

--- src/components/ui/password-input/index.tsx
+++ src/components/ui/password-input/index.tsx
@@ -1,10 +1,12 @@
 import { InputHTMLAttributes } from "react";
+import { LuEye } from 'react-icons/lu'
 import style from './index.module.css'
 
 export function PasswordInput({...props}:InputHTMLAttributes<HTMLInputElement>){
     return (
-        <div>
-            <input className={style.passwordInput} {...props} type="password" />
+        <div className={style.passwordDiv}>
+            <input className={style.passwordInput}  {...props} type="password" />
+            <LuEye className={style.passwordIcon}/>
         </div>
     )
 
